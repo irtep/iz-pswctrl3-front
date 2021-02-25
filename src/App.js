65ef0feda0b0a9fa98f262eb875f15230b48438e
irtep <rasanen.petri@gmail.com>
2021-02-25 21:44:16 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  useEffect( () => {
+    console.log('jojojojojo');
+    axios.get('/ping');
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
